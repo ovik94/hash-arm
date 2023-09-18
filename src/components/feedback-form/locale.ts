6aa107a99da566c3a62f634ff847b439a99d4fb3
bagdasarjan <o.bagdasarjan@cft.ru>
2023-09-19 00:35:44 +0700

    add feedback

--- src/components/feedback-form/locale.ts
+++ src/components/feedback-form/locale.ts
@@ -2,7 +2,8 @@ const Locale = {
   buttonLabel: 'Далее',
   backButtonLabel: 'Назад',
   selectOtherVariant: 'Ввести свой вариант',
-  selectOtherVariantLabel: 'Свой вариант'
+  selectOtherVariantLabel: 'Свой вариант',
+  messageLabel: 'Введите текст'
 };
 
 export default Locale;
