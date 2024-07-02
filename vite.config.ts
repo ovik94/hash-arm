0ae991a58367cd53660b8a7182a20c6faa29842b
Ovik Bagdasaryan <you@example.com>
2024-07-02 20:48:22 +0700

    Добавил новый тип выписки

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,58 @@
-import { defineConfig, loadEnv, splitVendorChunkPlugin, normalizePath } from 'vite';
-import react from '@vitejs/plugin-react';
-import checker from 'vite-plugin-checker'
-import { resolve } from 'path';
-import viteCompression from 'vite-plugin-compression';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import progress from 'vite-plugin-progress'
+import {
+  defineConfig,
+  loadEnv,
+  splitVendorChunkPlugin,
+  normalizePath,
+} from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+import { resolve } from "path";
+import viteCompression from "vite-plugin-compression";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import progress from "vite-plugin-progress";
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    base: '/',
+    base: "/",
     server: {
-      port: 80,
+      port: 3000,
       proxy: {
         // with options
-        '/api': {
+        "/api": {
           target: process.env.VITE_DEV_API_URL,
           secure: false,
-          changeOrigin: true
-        }
-      }
+          changeOrigin: true,
+        },
+      },
     },
-    publicDir: './public',
+    publicDir: "./public",
     build: {
       emptyOutDir: true,
       copyPublicDir: false,
       rollupOptions: {
         treeshake: true,
         output: {
-          dir: './dist'
+          dir: "./dist",
         },
-      }
+      },
     },
     plugins: [
       react(),
       checker({ typescript: true }),
       progress(),
-      mode === 'production' && viteCompression(),
-      mode === 'production' && splitVendorChunkPlugin(),
-      mode === 'production' && viteStaticCopy({
-        targets: [
-          {
-            src: normalizePath(resolve(__dirname, 'public')) + '/[!.]*',
-            dest: normalizePath(resolve(__dirname, 'dist', 'public')),
-          },
-        ],
-      })
-    ]
+      mode === "production" && viteCompression(),
+      mode === "production" && splitVendorChunkPlugin(),
+      mode === "production" &&
+        viteStaticCopy({
+          targets: [
+            {
+              src: normalizePath(resolve(__dirname, "public")) + "/[!.]*",
+              dest: normalizePath(resolve(__dirname, "dist", "public")),
+            },
+          ],
+        }),
+    ],
   });
-})
+});
